9da2331e5b706948e106549771d5f6bcb7afe685
heyeepc <2675458141@qq.com>
2025-11-02 14:52:43 +0800

    Update and rename 004.print()改变分隔符.py to 005.print()改变分隔符.py

--- 03.官方函数/005.print()改变分隔符.py
+++ 03.官方函数/005.print()改变分隔符.py
@@ -1,7 +1,7 @@
 # 默认行为（sep=' '）
-print("Hello", "World", 2025)
+print("Hello"， "World", 2025)
 # 输出: Hello World 2025
 
 # 使用自定义分隔符
-print("User", "Log", "Time", sep='|')
+print("User"， "Log", "Time", sep='|')
 # 输出: User|Log|Time
