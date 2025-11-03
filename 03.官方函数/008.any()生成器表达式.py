8211d3a7e8f51337f5502e34e94c93208dd530b5
heyeepc <2675458141@qq.com>
2025-11-03 21:27:55 +0800

    Update 008.any()生成器表达式.py

--- 03.官方函数/008.any()生成器表达式.py
+++ 03.官方函数/008.any()生成器表达式.py
@@ -3,3 +3,9 @@ numbers = [1, 5, 12, 8, 3]
 # 生成器表达式：(x > 10 for x in numbers) 会产生 (False, False, True, False, False)
 result = any(x > 10 for x in numbers)
 print(result)  # 输出: True
+
+
+s = "helloWorld"
+# 检查每个字符是否为大写，然后 any() 检查结果
+result = any(char.isupper() for char in s)
+print(result)  # 输出: True (因为 'W' 是大写)
