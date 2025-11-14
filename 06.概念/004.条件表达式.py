e020836f7c8d4a80d970b748ae0e17f1258ec277
heyeepc <2675458141@qq.com>
2025-11-14 11:05:18 +0800

    Create 004.条件表达式.py

---
+++ 06.概念/004.条件表达式.py
@@ -0,0 +1,16 @@
+a = 10
+b = 25
+
+# Python 条件表达式：如果 a > b 为真，结果是 a；否则是 b
+max_value = a if a > b else b
+
+print(f"两个数中的最大值是: {max_value}")
+# 输出: 两个数中的最大值是: 25
+
+num = 7
+
+# Python 条件表达式：如果 num % 2 == 0 为真，结果是 "偶数"；否则是 "奇数"
+result = "偶数" if num % 2 == 0 else "奇数"
+
+print(f"{num} 是一个 {result}")
+# 输出: 7 是一个 奇数
