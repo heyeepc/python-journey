3f936809c680515b3a0b5c5f5f4f43d05493e5fb
heyeepc <2675458141@qq.com>
2025-11-18 00:39:02 +0800

    Update 010.enumerate().py

--- 03.官方函数/010.enumerate().py
+++ 03.官方函数/010.enumerate().py
@@ -6,3 +6,12 @@ for index, num in enumerate(numbers):
     if num == 30:
         print(f"数字30的索引是: {index}")
         break
+
+my_list = ['A', 'B', 'C']
+for index, item in my_list:  # 直接解包获取索引和值
+    print(f"索引 {index}: 元素 {item}")
+
+# 输出：
+# 索引 0: 元素 A
+# 索引 1: 元素 B
+# 索引 2: 元素 C
