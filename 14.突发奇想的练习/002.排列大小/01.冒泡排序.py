0a471887383007c6d88a894e100ec91fbaec6606
heyeepc <2675458141@qq.com>
2026-09-04 09:35:30 +0800

    Update 01.冒泡排序.py

--- 14.突发奇想的练习/002.排列大小/01.冒泡排序.py
+++ 14.突发奇想的练习/002.排列大小/01.冒泡排序.py
@@ -1,1 +1,12 @@
+lst = [64, 34, 25, 12, 22, 11, 90, 88, 120, 130]
 
+for i in range(10):
+    swapped = False
+    for j in range(0,10 - i - 1):
+        if lst[j] > lst[j+1]:
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+            swapped = True
+    if not swapped:
+        break
+
+print(lst)
